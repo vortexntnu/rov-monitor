bb1d0590418b8ab47c02c4ab1229ce1c80f92587
Sindre Hansen <sindrehansen92@gmail.com>
2018-06-13 23:50:21 +0200

    Fix indentation

--- monitor-client/scripts/monitor-client.py
+++ monitor-client/scripts/monitor-client.py
@@ -13,39 +13,40 @@ class MonitorClientNode(object):
                                                  DeviceStatus,
                                                  queue_size=1)
 
-        def get_ip():
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect(("192.168.0.1", 80))
-            ip = s.getsockname()[0]
-            s.close()
-            return ip
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("192.168.0.1", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
 
-        def get_cpu_temp():
-            try:
-                with open('/sys/class/thermal/thermal_zone0/temp') as temp_file:
-                    temp = int(temp_file.read())
-                    return temp/1000
-            except:
-                return -1
+    def get_cpu_temp(self):
+        try:
+            with open('/sys/class/thermal/thermal_zone0/temp') as temp_file:
+                temp = int(temp_file.read())
+                return temp/1000
+        except:
+            return -1
 
-        def get_leak_status():
-            return -1  # TODO: Check if leak sensor is available, read if it is
+    def get_leak_status(self):
+        return -1  # TODO: Check if leak sensor is available, read if it is
 
-        def get_id():
-            try:
-                with open('/sys/block/mmcblk0/device/cid') as id_file:
-                    device_id = id_file.read()
-            except:
-                device_id = "N/A"
-            return device_id
+    def get_id(self):
+        try:
+            with open('/sys/block/mmcblk0/device/cid') as id_file:
+                device_id = id_file.read()
+        except:
+            rospy.logwarn_throttle(120, "Failed to get device identifier")
+            device_id = "N/A"
+        return device_id
 
-        def get_type():
-            try:
-                device_type = os.environ["VORTEX_DEVICE_TYPE"]
-            except KeyError:
-                rospy.roswarn("Failed to get device type")
-                device_type = "Unknown"
-            return device_type
+    def get_type(self):
+        try:
+            device_type = os.environ["VORTEX_DEVICE_TYPE"]
+        except KeyError:
+            rospy.logwarn_throttle(120, "Failed to get device type")
+            device_type = "Unknown"
+        return device_type
 
     def publish_status(self):
         status_msg = DeviceStatus()
