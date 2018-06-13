1f31febc067327313e443a1380a522f4b7e2f25b
Sindre Hansen <sindrehansen92@gmail.com>
2018-06-13 23:42:39 +0200

    Add class for monitor-server
    Gathers the devices publishing on /device_status and groups them into a
    single topic; /device_list

--- monitor-server/scripts/monitor-server.py
+++ monitor-server/scripts/monitor-server.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python
+import rospy
+
+from monitor_msgs.msg import DeviceList, DeviceStatus
 
 
-def main():
-    return 0
+class MonitorServerNode(object):
+    def __init__(self):
+        rospy.init_node("monitor_server_node")
+        self.devices = {}
+        self.pub_device_list = rospy.Publisher('device_list',
+                                               DeviceList,
+                                               queue_size=10)
+        self.sub_statuses = rospy.Subscriber('device_status',
+                                             DeviceStatus,
+                                             self.device_status_cb,
+                                             queue_size=10)
+
+    def publish_devices(self):
+        device_list_msg = DeviceList()
+        for ip in self.devices:
+            device_list_msg.devices.append(self.devices[ip])
+        device_list_msg.header.stamp = rospy.get_rostime()
+        self.pub_device_list.publish(device_list_msg)
+
+    def device_status_cb(self, device):
+        self.devices[device.device_id] = device
 
 
 if __name__ == '__main__':
-    main()
+    monitor_server_node = MonitorServerNode()
+    while not rospy.is_shutdown():
+        monitor_server_node.publish_devices()
+        rospy.Rate(1).sleep()
